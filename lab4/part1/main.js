19196c2ec9038b94d13fe7f8ac44daf679e7bd9e
matt-311 <matthew.pearce1@dcmail.ca>
2025-03-11 12:42:20 -0400

    Create the variables in the JavaScript file

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -6,3 +6,8 @@ function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertx = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const inserty = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertz = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
