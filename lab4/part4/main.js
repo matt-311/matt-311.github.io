d44caa0724c9e50ecdb21f281b8f91a9340196f4
matt-311 <matthew.pearce1@dcmail.ca>
2025-03-18 14:30:02 -0400

    Create the evil circle
    Created a new evil circle object, and updated the loop method code

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -158,14 +158,21 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evilCircle = new EvilCircle(random(0, width), random(0, height));
+
 function loop() {
   ctx.fillStyle = "rgb(0 0 0 / 25%)";
   ctx.fillRect(0, 0, width, height);
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
   }
+  evilCircle.draw();
+  evilCircle.update();
+  evilCircle.collisionDetect();
   requestAnimationFrame(loop);
 }
 
