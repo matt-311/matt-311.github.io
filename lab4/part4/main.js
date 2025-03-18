9823ba27313904b5a70504328b49e457f7ba9220
matt-311 <matthew.pearce1@dcmail.ca>
2025-03-18 14:12:22 -0400

    Created the EvilCircle class
    Created the EvilCircle class with a constructor that accepts x and y values, and inherits from the shape class. Also added code to allow the EvilCircle to be controlled with wsad.

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -83,6 +83,31 @@ class Ball extends Shape{
   }
 }
 
+// Create the Evil Circle class
+class EvilCircle extends shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.colour = "white";
+    this.size = 10;
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+}
+
 // Animating the ball
 const balls = []
 
