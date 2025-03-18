68649a15b717b5dad481ebc860a4daffd52d54a1
matt-311 <matthew.pearce1@dcmail.ca>
2025-03-18 13:35:55 -0400

    Started writing the animation code
    Started writing the code that creates the balls and sets their values

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -61,3 +61,19 @@ class Ball {
     this.y += this.velY;
   }
 }
+
+// Animating the ball
+const balls = []
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+  balls.push(ball);
+}
