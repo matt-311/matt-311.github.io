c21e0b9c7a850a44f92aed83ddafb389431ea765
matt-311 <matthew.pearce1@dcmail.ca>
2025-03-18 12:31:32 -0400

    Created an array for the alternative text
    Created an array that holds alternative text for each image file

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -17,6 +17,12 @@ const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic4.jp
 
 /* Declaring the alternative text for each image file */
 
+const alternativeText = {'pic1.jpg':'A picture of a human eye', 
+    'pic2.jpg':'An image of a rock with a wavey texture', 
+    'pic3.jpg':'An image of white a purple flowers', 
+    'pic4.jpg':'An image of drawings on a wall of a tomb in ancient Egypt', 
+    'pic4.jpg':'A moth sitting on a leaf'}
+
 /* Looping through images */
 
 const newImage = document.createElement('img');
