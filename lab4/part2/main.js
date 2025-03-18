8b24ebacdc209c8b1abb648949ff5682ae7259d8
matt-311 <matthew.pearce1@dcmail.ca>
2025-03-18 12:49:41 -0400

    Implemented looping through images
    Coded functionality for looping through images and their alt text, and fixed some things

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -13,7 +13,7 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
-const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic4.jpg']
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 /* Declaring the alternative text for each image file */
 
@@ -21,13 +21,18 @@ const alternativeText = {'pic1.jpg':'A picture of a human eye',
     'pic2.jpg':'An image of a rock with a wavey texture', 
     'pic3.jpg':'An image of white a purple flowers', 
     'pic4.jpg':'An image of drawings on a wall of a tomb in ancient Egypt', 
-    'pic4.jpg':'A moth sitting on a leaf'}
+    'pic5.jpg':'A moth sitting on a leaf'}
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const file of imageFilenames) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${file}`);
+    newImage.setAttribute('alt', alternativeText[file]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', click => {displayedImage.src = click.target.src;
+        displayedImage.alt = click.target.alt;
+    })
+}
 
 /* Wiring up the Darken/Lighten button */
